53d5bf30b81db640e898e1a52bb65171f3ace96b
Idris Raja <idris.raja@gmail.com>
2023-07-04 12:51:08 -0500

    comment out thing not working

--- flake.nix
+++ flake.nix
@@ -6,11 +6,12 @@
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs { system = system; };
     in {
+      apps.${system} = { };
       overlays = {
         dimensions = import ./dimensions.nix;
         mdtopdf = import ./mdtopdf.nix;
         newcover = import ./newcover.nix;
-        roamamer = import ./roamamer.nix;
+        # roamamer = import ./roamamer.nix;
         seder = import ./seder.nix;
         transcribe = import ./transcribe.nix;
       };
