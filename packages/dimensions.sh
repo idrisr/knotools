d6471cdac789101c8e6458a18c59b655a8bacb2c
Idris Raja <idris.raja@gmail.com>
2023-12-28 08:04:16 -0600

    update dimension logic

--- packages/dimensions.sh
+++ packages/dimensions.sh
@@ -10,4 +10,9 @@ if [[ "$#" != 1 ]]; then
   usage
 fi
 
-pdftk "$1" dump_data_utf8 | rg --text dimension | sort -u
+input=$(realpath "$1")
+dims=$(pdftk "$input" dump_data_utf8 | rg --text pagemediacroprect | sort -u | head -n1)
+width=$(echo "$dims" | awk '{print $4}')
+height=$(echo "$dims" | awk '{print $5}')
+
+echo "${width%.*} ${height%.*}"
